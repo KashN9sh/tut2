f5bb6fb2c731e9e55f33fb52da2ba7c736ed1660
KashN9sh <kash244@yandex.ru>
2020-05-03 15:57:43 +0300

    drop_css

--- components/dropdown/dropdown.js
+++ components/dropdown/dropdown.js
@@ -1,11 +1,38 @@
 window.onload = function() {
   let dropdowns = document.getElementsByClassName("dropbtn");
-  var i;
-  for ( i = 0; i < dropdowns.length; i++) {
-    dropdowns[i].id=i;
+  let drop_icon = document.getElementsByClassName("drop_icon");
+  let id=0;
+  for (let i = 0; i < dropdowns.length; i++) {
+    dropdowns[i].id=id;
+    drop_icon[i].id=id;
+    if (dropdowns[i].classList.contains('date')){
+      dropdowns[i+1].id=id;
+      drop_icon[i+1].id=id;
+      i++;
+      id++;
+    }
+    id++;
   }
 }
 function drop(id) {
+  let dropdown = document.getElementsByClassName("dropdown")[id];
   let content = document.getElementsByClassName("dropdown-content")[id];
-  content.classList.toggle('show');
+  if(dropdown.classList.contains('date')){
+    content.classList.toggle('show');
+  }
+  else{
+    content.classList.toggle('show');
+    dropdown.classList.toggle('open');
+  }
+}
+
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn') && !event.target.matches('.drop_icon')) {
+    let dropdowns = document.getElementsByClassName("dropdown-content");
+    for (let i = 0; i < dropdowns.length; i++) {
+      if (dropdowns[i].classList.contains('show')) {
+        dropdowns[i].classList.remove('show');
+      }
+    }
+  }
 }
