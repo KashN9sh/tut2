99bad70c438826decbb789ae4b70c8b0218338c8
KashN9sh <kash244@yandex.ru>
2020-05-08 18:20:12 +0300

    dropdown_long_comlete

--- components/dropdown/dropdown.js
+++ components/dropdown/dropdown.js
@@ -17,9 +17,54 @@ function drop(id) {
     dropdown.classList.toggle('open');
   }
 }
+function plus(id){
+  let count=document.getElementsByClassName('count');
+  count[id].textContent=Number(count[id].textContent)+1;
+  guests();
+  if(Number(count[id].textContent)>0){
+    let minus=document.getElementsByClassName('minus');
+    minus[id].classList.remove('zero')
+  }
+  else{
+    minus[id].classList.add('zero')
+  }
+}
+function minus(id){
+  let count=document.getElementsByClassName('count');
+  if(Number(count[id].textContent)>0){
+    count[id].textContent=Number(count[id].textContent)-1;
+    guests();
+  }
+  if(Number(count[id].textContent)==0){
+    let minus=document.getElementsByClassName('minus');
+    minus[id].classList.add('zero')
+  }
+
+}
+function guests(){
+  document.getElementsByClassName('clear')[0].classList.remove('inviz');
+  let guests=document.getElementsByClassName('guests');
+  let count=document.getElementsByClassName('count');
+  let num=0;
+  for (let i = 0; i < 3; i++){
+    num=num+Number(count[i].textContent);
+  }
+  if(num>1) guests[0].innerHTML=num+" гостя";
+  else if(num<1) guests[0].innerHTML=num+" гостей";
+  else guests[0].innerHTML=num+" гость";
+}
+
+function clс(){
+  let count=document.getElementsByClassName('count');
+  for (let i = 0; i < 3; i++){
+    count[i].textContent=0;
+  }
+  guests();
+  document.getElementsByClassName('clear')[0].classList.add('inviz');
+}
 
 window.onclick = function(event) {
-  if (!event.target.matches('.dropbtn') && !event.target.matches('.drop_icon')) {
+  if (event.target.matches('.ok')) {
     let content = document.getElementsByClassName("dropdown-content");
     let dropdown = document.getElementsByClassName("dropdown");
     for (let i = 0; i < content.length; i++) {
