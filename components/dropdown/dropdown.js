455a140a45827fd0c06ff441fe0cf8daff8bc3cf
KashN9sh <kash244@yandex.ru>
2020-05-09 22:15:15 +0300

    dropdowns_complete

--- components/dropdown/dropdown.js
+++ components/dropdown/dropdown.js
@@ -20,7 +20,8 @@ function drop(id) {
 function plus(id){
   let count=document.getElementsByClassName('count');
   count[id].textContent=Number(count[id].textContent)+1;
-  guests();
+  if(id<3) guests();
+  else comfort();
   if(Number(count[id].textContent)>0){
     let minus=document.getElementsByClassName('minus');
     minus[id].classList.remove('zero')
@@ -33,7 +34,8 @@ function minus(id){
   let count=document.getElementsByClassName('count');
   if(Number(count[id].textContent)>0){
     count[id].textContent=Number(count[id].textContent)-1;
-    guests();
+    if(id<3) guests();
+    else comfort();
   }
   if(Number(count[id].textContent)==0){
     let minus=document.getElementsByClassName('minus');
@@ -42,7 +44,10 @@ function minus(id){
 
 }
 function guests(){
-  document.getElementsByClassName('clear')[0].classList.remove('inviz');
+  let clear=document.getElementsByClassName('clear');
+  if(clear.length!=0){
+    clear[0].classList.remove('inviz');
+  }
   let guests=document.getElementsByClassName('guests');
   let count=document.getElementsByClassName('count');
   let num=0;
@@ -52,6 +57,25 @@ function guests(){
   if(num>1) guests[0].innerHTML=num+" гостя";
   else if(num<1) guests[0].innerHTML=num+" гостей";
   else guests[0].innerHTML=num+" гость";
+}
+
+function comfort(){
+  let comfort=document.getElementsByClassName('comforts');
+  let count=document.getElementsByClassName('count');
+  let sl,bd,bh;
+  if(Number(count[3].textContent==1)) sl="1 спальня";
+  else if(Number(count[3].textContent==0)) sl="0 спален";
+  else sl=count[3].textContent+" спальни";
+
+  if(Number(count[4].textContent==1)) bd="1 кровать";
+  else if(Number(count[4].textContent==0)) bd="0 кроватей";
+  else bd=count[4].textContent+" кровати";
+
+  if(Number(count[5].textContent==1)) bh="1 ванна";
+  else if(Number(count[5].textContent==0)) bh="0 ванных";
+  else bh=count[5].textContent+" ванных";
+
+  comfort[0].innerHTML=sl+", "+bd+", "+bh;
 }
 
 function clс(){
